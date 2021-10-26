07611d59b789a63735fa445b2091ee0b49335bb9
Brandon Cozart <bcozart@uncc.edu>
2021-10-26 17:21:18 -0400

    flaskApp03AddedExecutionPathForPOSTRequest

--- flask03.py
+++ flask03.py
@@ -1,6 +1,7 @@
 # FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
 
 # imports
+from flask import request
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template
@@ -19,24 +20,31 @@ def index():
 
 @app.route('/notes')
 def get_notes():
+    a_user = {'name': 'Brandon', 'email':'bcozart@uncc.edu'} #Here we added a variable (a_user) to store our mock user data and we passed that variable to our template view (index.html) with a label called user.# 
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
-    return render_template('notes.html', notes=notes)
+    return render_template('notes.html', notes=notes , user = a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
+    a_user = {'name': 'Brandon', 'email':'bcozart@uncc.edu'}
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
-    return render_template('note.html', note=notes[int(note_id)])
+    return render_template('note.html', note=notes[int(note_id)], user = a_user)
 
-@app.route('/notes/new')
+@app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
     a_user = {'name': 'Brandon', 'email':'bcozart@uncc.edu'} #Here we added a variable (a_user) to store our mock user data and we passed that variable to our template view (index.html) with a label called user.#
-    return render_template('new.html', user = a_user)
+    print('request method is', request.method)
+    if request.method == 'POST':
+        return '<h1> POST method used for this request </h1>'
+    else:
+
+        return render_template('new.html', user = a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
