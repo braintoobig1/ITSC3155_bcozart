c035b8beaa694f15f7bf44b396f9aeff8ae76579
Brandon Cozart <bcozart@uncc.edu>
2021-11-03 07:03:10 -0400

    Flask04ReturnOne

--- flask04.py
+++ flask04.py
@@ -30,25 +30,26 @@ with app.app_context():
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
+@app.route('/')
 @app.route('/index')
 def index():
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu') #Here we added a variable (a_user) to store our mock user data and we passed that variable to our template view (index.html) with a label called user.# 
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one() #Here we added a variable (a_user) to store our mock user data and we passed that variable to our template view (index.html) with a label called user.# 
 
     return render_template('index.html' , user = a_user)
 
 @app.route('/notes')
 def get_notes():
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu') #Here we added a variable (a_user) to store our mock user data and we passed that variable to our template view (index.html) with a label called user.# 
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one() #Here we added a variable (a_user) to store our mock user data and we passed that variable to our template view (index.html) with a label called user.# 
     #here
     my_notes = db.session.query(Note).all()
-    return render_template('notes.html', notes=notes , user = a_user)
+    return render_template('notes.html', notes=my_notes , user = a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
     #here
-    my_note = db.session.query(Note).filter_by(id=note_id)
-    return render_template('note.html', note=notes[int(note_id)], user = a_user)
+    my_note = db.session.query(Note).filter_by(id=note_id).one()
+    return render_template('note.html', note=my_note, user = a_user)
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
@@ -66,7 +67,7 @@ def new_note():
 
         return redirect(url_for('get_notes'))
     else:
-        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
         return render_template('new.html', user = a_user)
 
 
